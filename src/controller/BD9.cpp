3b14e48318632cde43c64f171f2447814e9bf76f
Ross Fish <moffenzeefmodular@gmail.com>
2025-04-07 16:00:16 -0700

    fixed param labels for both channels

--- src/controller/BD9.cpp
+++ src/controller/BD9.cpp
@@ -4,11 +4,19 @@
 
 BD9Module::BD9Module( ) {
   config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-  configParam(DRUM_PARAM, 0.0, 15.0, 7.0, "Sample");
-  configParam(DRUM_PARAM + 1, 0.0, 15.0, 7.0, "Sample");
+  configParam(DRUM_PARAM, 0.0, 15.0, 7.0, "Sample Select Ch. 1");
+  configParam(TUNE_PARAM, 0.2, 1.8, 1, "Playback Speed Ch. 1", "x");
+  configInput(DRUM_CV, "Select CV Ch. 1");
+  configInput(GATE_INPUT, "Gate Input Ch. 1");
+  configInput(TUNE_CV, "Speed CV Ch. 1");
+  configOutput(AUDIO_OUTPUT, "Ch. 1");
 
-  configParam(TUNE_PARAM, 0.2, 1.8, 1, "Playback Speed", "x");
-  configParam(TUNE_PARAM + 1, 0.2, 1.8, 1, "Playback Speed", "x");
+  configParam(DRUM_PARAM + 1, 0.0, 15.0, 7.0, "Sample Select Ch. 2");
+  configParam(TUNE_PARAM + 1, 0.2, 1.8, 1, "Playback Speed Ch. 2", "x");
+  configInput(DRUM_CV + 1, "Select CV Ch. 2");
+  configInput(GATE_INPUT + 1, "Gate Input Ch. 2");
+  configInput(TUNE_CV + 1, "Speed CV Ch. 2");
+  configOutput(AUDIO_OUTPUT + 1, "Ch. 2");
 
   numModules = 2;
 
